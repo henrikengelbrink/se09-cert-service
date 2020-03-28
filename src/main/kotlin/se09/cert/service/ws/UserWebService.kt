3b8599f53cb4204d049babb7eba0317e4e7d229a
henrikengelbrink <henrik@keeet.io>
2020-03-29 00:15:53 +0100

    fix: Add user mqtt register

---
+++ src/main/kotlin/se09/cert/service/ws/UserWebService.kt
@@ -0,0 +1,38 @@
+package se09.cert.service.ws
+
+import io.micronaut.context.annotation.Value
+import io.micronaut.http.HttpRequest.POST
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.MediaType
+import io.micronaut.http.client.RxHttpClient
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import se09.device.service.dto.VerneMQRegisterDTO
+import java.net.URL
+import javax.inject.Singleton
+
+@Singleton
+class UserWebService {
+
+    @Value("\${service.url.user}")
+    private lateinit var userServiceUrl: String
+
+    private val LOG: Logger = LoggerFactory.getLogger(UserWebService::class.java)
+
+    fun loginValid(dto: VerneMQRegisterDTO): Boolean {
+        LOG.info("loginValid")
+        val httpClient = RxHttpClient.create(URL(userServiceUrl))
+        val payload = mapOf(
+                "username" to dto.username,
+                "password" to dto.password,
+                "clientId" to dto.clientId
+        )
+        val response = httpClient.toBlocking().exchange(
+                POST("/mqtt/register", payload).contentType(MediaType.APPLICATION_JSON),
+                Map::class.java
+        )
+        LOG.info("loginValid status: ${response.status.code}")
+        return response.status == HttpStatus.OK
+    }
+
+}
